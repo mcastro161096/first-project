6d795037fb2215cbf039e1aac976fe316fbfb894
Matheus Castro <MATHEUSCASTRO161096@gmail.com>
2022-09-26 22:25:57 -0300

    Using body params with body-parser.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser')
 const greeting = require('./Middleware')
 app.use('/mid', greeting)
 
-
+app.use(bodyParser.text())
+app.use(bodyParser.json())
 app.get("/list", (req, res) => {
 res.json({
-    name: 'iPad 32Gb',
+    name: 'iPad 32Gb', 
     price: 1889.00
 })
 })
@@ -20,7 +21,10 @@ app.get('/customers/:id', (req, res) => {
     res.send(`Customer ${req.params.id} selected!`)
 })
 
+app.post('/body', (req, res) => {
+    res.send(req.body)
+})
 
 app.listen(3000, () => {
-    console.log('backend tá executando...')
+    console.log('backend executing...')
 })
